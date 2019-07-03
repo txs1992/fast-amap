ddf44a4db532f54e70a7c5ecc901e2496bae433f
txs1992 <xusheng.txs@gmail.com>
2019-07-03 20:24:06 +0800

    fix typo

--- tests/unit/amap.spec.ts
+++ tests/unit/amap.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { mount, shallowMount } from "@vue/test-utils";
-import FastMap from "packages/components/map/index.vue";
+import FastMap from "packages/components/map/map.vue";
 import MapOptions from "packages/utils/map-options";
 
 describe("AMap.vue", () => {
@@ -18,7 +18,7 @@ describe("AMap.vue", () => {
 
   it("renders the correct markup", () => {
     expect(wrapper.html()).contain(
-      '<div class="cpt-fast-map" style="height: 600px;"><div class="fast-map-slot-container"></div></div>'
+      '<div class="cpt-fast-map" style="height: 600px;"><div class="fast-map-slot-container"><!----></div></div>'
     );
   });
 });
