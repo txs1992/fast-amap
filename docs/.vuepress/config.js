104d9ee3828669805bfa3e2a8b5477cb667e3606
txs1992 <xusheng.txs@gmail.com>
2019-07-06 14:37:10 +0800

    feature：添加 Fast AMap 介绍文档。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  base: '/fast-amap/',
+  repo: 'https://github.com/txs1992/fast-amap',
   locales: {
     '/': {
       lang: 'zh-CN',
@@ -12,9 +14,34 @@ module.exports = {
     }
   },
   themeConfig: {
-    nav: [
-      { text: '指南', link: '/guide/' },
-      { text: '组件', link: '/components/' }
-    ]
+    locales: {
+      '/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        nav: [
+          { text: '指南', link: '/guide/' },
+          { text: '组件', link: '/components/' }
+        ],
+        sidebar: 'auto'
+        // sidebar: {
+        //   '/guide/': [
+        //     {
+        //       title: '介绍',
+        //       collapsable: false,
+        //       sidebar: 'auto',
+        //       children: [
+        //         { text: '指南', link: '/guide/' },
+        //         '',
+        //         'getting-started',
+        //         'directory-structure',
+        //         'basic-config'
+        //       ]
+        //     }
+        //   ]
+        // }
+      }
+    }
   }
 }
