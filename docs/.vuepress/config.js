184417883ed8fb41620033b4f8c23debf1a2f9ce
txs1992 <xusheng.txs@gmail.com>
2019-07-06 12:41:49 +0800

    feature：添加 Fast AMap 文档基础配置。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  title: 'FastAMap',
-  description: 'Just playing around'
+  themeConfig: {
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        title: 'FastAMap',
+        description: '基于 Vue 的高性能高德地图件库'
+      },
+      '/en/': {
+        lang: 'en-US',
+        title: 'FastAMap',
+        description: 'Vue-based High Performance Gaud Map Library'
+      }
+    },
+
+    nav: [{ text: 'Home', link: '/' }, { text: 'Guide', link: '/guide/' }]
+  }
 }
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,1 @@
+export default function({ Vue, options, router, siteData }) {}
