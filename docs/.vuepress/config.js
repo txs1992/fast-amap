e6b8bab6a9d3587a9d3b74d75ccb718d9c88f1be
txs1992 <xusheng.txs@gmail.com>
2019-07-06 12:47:26 +0800

    feature：修改配置，添加组件导航。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  themeConfig: {
-    locales: {
-      '/': {
-        lang: 'zh-CN',
-        title: 'FastAMap',
-        description: '基于 Vue 的高性能高德地图件库'
-      },
-      '/en/': {
-        lang: 'en-US',
-        title: 'FastAMap',
-        description: 'Vue-based High Performance Gaud Map Library'
-      }
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'FastAMap',
+      description: '基于 Vue 的高性能高德地图件库'
     },
-
-    nav: [{ text: 'Home', link: '/' }, { text: 'Guide', link: '/guide/' }]
+    '/en/': {
+      lang: 'en-US',
+      title: 'FastAMap',
+      description: 'Vue-based High Performance Gaud Map Library'
+    }
+  },
+  themeConfig: {
+    nav: [
+      { text: '指南', link: '/guide/' },
+      { text: '组件', link: '/components/' }
+    ]
   }
 }
