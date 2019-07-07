08fc6b7c57275931f3202d66a7b5c7b2ba88a180
txs1992 <xusheng.txs@gmail.com>
2019-07-07 12:45:22 +0800

    feature：添加快速上手文档。

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './app.vue'
 import router from './router'
 
 // import FastAMap from '../lib/fast-amap.mini'
-// import FastAMap from '../packages/index'
+import FastAMap from '../packages/index'
 
 FastAMap.mapOptions.setOptions({
   key: 'd2d76e2274bf5973ecfb1f68454b6f3b',
