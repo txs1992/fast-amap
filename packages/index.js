b8fc811d55d317cf0a3f9d175a04b8176bd64c95
txs1992 <xusheng.txs@gmail.com>
2019-07-18 14:17:23 +0800

    feature：修复入口文件。

--- packages/index.js
+++ packages/index.js
@@ -1,20 +1,28 @@
 import FastMap from './components/map/index'
 import FastText from './components/text/index'
 import FastMarker from './components/marker/index'
-import FastPolygons from './components/polygons/index'
+import FastPolygon from './components/polygons/index'
+import FastPolyline from './components/polyline/index'
 
 import MapOptions from './utils/map-options'
 import mapLoader, { mapOptionLoader } from './utils/map-loader'
 import MapRegistry from './utils/map-instance-registry'
 
+const components = [FastMap, FastText, FastMarker, FastPolyline, FastPolygon]
+
 function install(Vue) {
-  ;[FastMap, FastText, FastMarker, FastPolygons].forEach(cpt =>
-    cpt.install(Vue)
-  )
+  components.forEach(cpt => cpt.install(Vue))
 }
 
 const registry = MapRegistry.getRegistryInstance()
 
 const mapOptions = MapOptions.getOptionsInstance()
 
-export default { install, registry, mapOptions, mapLoader, mapOptionLoader }
+export default {
+  install,
+  registry,
+  mapOptions,
+  mapLoader,
+  mapOptionLoader,
+  ...components
+}
