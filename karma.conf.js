ea11843c596c8a7925ed2a286b5849dc6dec0400
txs1992 <xusheng.txs@gmail.com>
2019-07-23 15:49:37 +0800

    feature：按照 headless-karma-travis 找个仓库，重新修改 karma 配置。

--- karma.conf.js
+++ karma.conf.js
@@ -51,15 +51,29 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless', 'MyHeadlessChrome'],
+
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--disable-translate',
+          '--disable-extensions',
+          '--no-first-run',
+          '--disable-background-networking',
+          '--remote-debugging-port=9223'
+        ]
+      }
+    },
+
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 3,
     browserNoActivityTimeout: 60000,
-    flags: ['--disable-web-security', '--disable-gpu', '--no-sandbox'],
+    // flags: ['--disable-web-security', '--disable-gpu', '--no-sandbox'],
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
