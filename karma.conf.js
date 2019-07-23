ba7139331439a86900e19107433d5a015d1f965b
txs1992 <xusheng.txs@gmail.com>
2019-07-23 16:25:49 +0800

    feature：修改 browsers 配置。

--- karma.conf.js
+++ karma.conf.js
@@ -54,11 +54,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'ChromeHeadless', 'MyHeadlessChrome'],
+    browsers: ['Chrome', 'MyHeadlessChrome'],
 
     customLaunchers: {
       MyHeadlessChrome: {
-        base: 'ChromeHeadless',
+        base: 'Chrome',
         flags: [
           '--no-sandbox'
           // '--disable-translate',
