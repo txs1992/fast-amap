b6ec1768c0f97fefd28d1cdfd78eb72599803c38
txs1992 <xusheng.txs@gmail.com>
2019-07-27 08:50:53 +0800

    feature：添加 marker 组件剩余测试功能。

--- test/marker.spec.js
+++ test/marker.spec.js
@@ -4,7 +4,7 @@ import FastAMap from '../packages'
 
 const { mapOptions, FastMap, FastMarker } = FastAMap
 
-function createMarker() {
+function createMarker(callback) {
   return mount(FastMarker, {
     propsData: {
       mid: 'marker',
@@ -23,6 +23,9 @@ function createMarker() {
       offset: [-13, -30],
       icon:
         '//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png'
+    },
+    listeners: {
+      click: callback ? callback : () => {}
     }
   })
 }
@@ -37,6 +40,27 @@ describe('FastMarker', () => {
     propsData: {
       mid: 'marker'
     }
+  })
+
+  it('test marker click events', done => {
+    mapWrapper.vm
+      .getAMapPromise()
+      .then(() => {
+        const wrapper = createMarker((event, map) => {
+          expect(event).to.be.an('object')
+          expect(map.CLASS_NAME).to.equal('AMap.Map')
+          expect(map).to.eql(mapWrapper.vm.getMapInstance())
+          done()
+        })
+        setTimeout(() => {
+          const marker = wrapper.vm.getMarkerByProp('myData', 1)
+          if (marker.bf.click[0].tb) {
+            // 模拟 marker 覆盖物点击事件
+            marker.bf.click[0].tb({ type: 'click' })
+          }
+        }, 0)
+      })
+      .catch(() => done(new Error()))
   })
 
   it('test getMarkerByProp function', done => {
@@ -70,5 +94,93 @@ describe('FastMarker', () => {
         }, 0)
       })
       .catch(() => done(new Error()))
+  })
+
+  it('test marker getAllMarkers', done => {
+    mapWrapper.vm
+      .getAMapPromise()
+      .then(() => {
+        const wrapper = createMarker()
+        setTimeout(() => {
+          const markers = wrapper.vm.getAllMarkers()
+          expect(markers).to.be.an('array')
+          expect(markers.length).to.be.equal(2)
+          expect(markers[0].CLASS_NAME).to.equal('AMap.Marker')
+          done()
+        }, 0)
+      })
+      .catch(() => done(new Error()))
+  })
+
+  it('test marker addMarkers', done => {
+    mapWrapper.vm
+      .getAMapPromise()
+      .then(() => {
+        const wrapper = createMarker()
+        setTimeout(() => {
+          const markers = wrapper.vm.getAllMarkers()
+          expect(markers.length).to.be.equal(2)
+
+          const options = [
+            {
+              myData: 3,
+              position: [121.472644, 31.231049]
+            },
+            {
+              myData: 4,
+              position: [121.482644, 31.231049]
+            }
+          ]
+
+          wrapper.vm.addMarkers(options)
+          expect(wrapper.vm.getAllMarkers().length).to.be.equal(4)
+          done()
+        }, 0)
+      })
+      .catch(() => done(new Error()))
+  })
+
+  it('test marker removeMarkers', done => {
+    mapWrapper.vm
+      .getAMapPromise()
+      .then(() => {
+        const wrapper = createMarker()
+        setTimeout(() => {
+          const markers = wrapper.vm.getAllMarkers()
+          expect(markers).to.be.an('array')
+          expect(markers.length).to.be.equal(2)
+          wrapper.vm.removeMarkers(markers, 'myData')
+          expect(wrapper.vm.getAllMarkers().length).to.be.equal(0)
+          done()
+        }, 0)
+      })
+      .catch(() => done(new Error()))
+  })
+
+  it('test marker clearAll', done => {
+    mapWrapper.vm
+      .getAMapPromise()
+      .then(() => {
+        const wrapper = createMarker()
+        setTimeout(() => {
+          const markers = wrapper.vm.getAllMarkers()
+          expect(markers).to.be.an('array')
+          expect(markers.length).to.be.equal(2)
+          wrapper.vm.clearAll()
+          expect(wrapper.vm.getAllMarkers().length).to.be.equal(0)
+          done()
+        }, 0)
+      })
+      .catch(() => done(new Error()))
+  })
+
+  it('test marker showAll', () => {
+    const wrapper = createMarker()
+    expect(wrapper.vm.showAll).to.be.an('function')
+  })
+
+  it('test marker hideAll', () => {
+    const wrapper = createMarker()
+    expect(wrapper.vm.hideAll).to.be.an('function')
   })
 })
